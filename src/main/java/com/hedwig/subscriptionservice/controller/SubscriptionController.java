9d973c4040c8c721e8722d3cfa082b50c4d80bf2
Ozan Aydın <ozanaydinn07@gmail.com>
2021-07-23 14:10:40 +0300

    Create subscription resource and implement create function

--- src/main/java/com/hedwig/subscriptionservice/controller/SubscriptionController.java
+++ src/main/java/com/hedwig/subscriptionservice/controller/SubscriptionController.java
@@ -4,19 +4,31 @@ import com.hedwig.subscriptionservice.common.SubscriptionServiceConstants;
 import com.hedwig.subscriptionservice.resource.SubscriptionResource;
 import com.hedwig.subscriptionservice.service.SubscriptionService;
 import com.sun.istack.NotNull;
+import io.netty.channel.ChannelOption;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.net.http.HttpClient;
+import java.time.Duration;
+import java.util.Map;
+import java.util.function.Predicate;
 
 @RestController
 @RequestMapping(SubscriptionServiceConstants.SUBSCRIPTION_SERVICE_PATH)
 public class SubscriptionController {
 
     private final SubscriptionService subscriptionService;
+    private final WebClient webClient;
 
     @Autowired
     public SubscriptionController(SubscriptionService subscriptionService){
+        this.webClient = WebClient.create(); // Can adjust HTTP functions, this is the simplest form of webclient
         this.subscriptionService = subscriptionService;
     }
 
@@ -25,7 +37,33 @@ public class SubscriptionController {
         //user ve product servicelere istek atılır.
         //confirmation alınır + user serviceden communication info alınır
         //kayıt atılır.
+    @PostMapping("/")
+    public ResponseEntity<String> createSubscription(@RequestParam Map<String, String> requestParams) {
+        String userId = requestParams.get("userId");
+        String productId = requestParams.get("productId");
 
+        // First communicate with userId to get user information.
+
+        //This statement will block until it gets a response.
+        // It will also throw a WebClientException if the response status is 4xx is 5xx.
+        String userResult = webClient.get()
+                .uri("localhost:8081")
+                .retrieve()
+                .bodyToMono(String.class).block();
+
+        // Now communicate with product to verify product ID
+        String productResult = webClient.get()
+                .uri("product URI")
+                .retrieve()
+                .bodyToMono(String.class).block();
+
+        //TODO connect with the database and update it
+
+        return new ResponseEntity<>("Subscription created!", HttpStatus.OK);
+
+    }
+
+    //TODO move to service
     //update sub
         //communication info güncellenir ve amqp mesajı gelir
         //db check edilir
@@ -40,7 +78,7 @@ public class SubscriptionController {
     @GetMapping("/user/{userId}")
     public ResponseEntity<?> getAllSubsForUser(@PathVariable(value = "userId") @NotNull final String userId){
 
-        SubscriptionResource subscriptionResource = subscriptionService.getSubsForUser(userId);
+        ResponseEntity<?> subscriptionResource = subscriptionService.getSubsForUser(userId);
 
         return ResponseEntity.status(HttpStatus.OK).body(subscriptionResource);
     }
@@ -49,7 +87,7 @@ public class SubscriptionController {
     @GetMapping("/product/{productId}")
     public ResponseEntity<?> getAllSubsForProduct(@PathVariable(value = "productId") final String productId){
 
-        SubscriptionResource subscriptionResource = subscriptionService.getSubsForProduct(productId);
+        ResponseEntity<?> subscriptionResource = subscriptionService.getSubsForProduct(productId);
 
         return ResponseEntity.status(HttpStatus.OK).body(subscriptionResource);
     }
--- src/main/java/com/hedwig/subscriptionservice/resource/SubscriptionResource.java
+++ src/main/java/com/hedwig/subscriptionservice/resource/SubscriptionResource.java
@@ -1,4 +1,28 @@
 package com.hedwig.subscriptionservice.resource;
 
+//TODO
 public class SubscriptionResource {
+    private String userId;
+    private String productId;
+
+    public SubscriptionResource(String userId, String productId) {
+        this.userId = userId;
+        this.productId = productId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
 }
