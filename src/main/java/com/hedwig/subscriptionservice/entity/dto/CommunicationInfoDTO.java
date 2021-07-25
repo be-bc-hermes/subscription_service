ba51dba75613938a0b685c34c2f97553ce43a9ee
snunal <nurunal196@gmail.com>
2021-07-26 00:57:05 +0300

    rabbitConfig added to handling JSON to object mapping

--- src/main/java/com/hedwig/subscriptionservice/entity/dto/CommunicationInfoDTO.java
+++ src/main/java/com/hedwig/subscriptionservice/entity/dto/CommunicationInfoDTO.java
@@ -1,12 +1,15 @@
 package com.hedwig.subscriptionservice.entity.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
 @Builder
+@NoArgsConstructor
 public class CommunicationInfoDTO implements Serializable {
 
     private Long id;
